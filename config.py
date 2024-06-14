5b258f9dcf92a8fa2c51afb5fae3a3ebd45b0850
emendezy <100findme@gmail.com>
2024-06-14 14:32:16 -0400

    break configs out into better files

--- config.py
+++ config.py
@@ -1,10 +1,24 @@
+"""Configuration file for the Jira Wrapped script."""
+import os
+from typing import Union
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def str2bool(boolstring: Union[str, bool]) -> bool:
+    if type(boolstring) is bool:
+        return boolstring
+    return boolstring.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh', 'alright', 'okay']
+
 #######################
 # SCRIPT CONFIGURATION
 #######################
-DEBUG = True  # If this is True, we only pull 1 day of issues instead of 1 year
-VERBOSE = True  # If this is True, we print out all the issue details
-LIST_CUSTOM_FIELDS = False  # If this is True, we list all the custom fields available in the Jira instance
-FILE_LOG_LINE_LENGTH = 120  # Max length of the lines that the logs wrap to
+DEBUG = str2bool(os.environ.get("DEBUG", True))  # If this is True, we only pull 1 day of issues instead of 1 year
+VERBOSE = str2bool(os.environ.get("VERBOSE", True))  # If this is True, we print out all the issue details
+LIST_CUSTOM_FIELDS = str2bool(os.environ.get("LIST_CUSTOM_FIELDS", False))  # If this is True, we list all the custom fields available in the Jira instance
+FILE_LOG_LINE_LENGTH = int(os.environ.get("FILE_LOG_LINE_LENGTH", 120))  # Max length of the lines that the logs wrap to
+
 IMPORTANT_CUSTOM_FIELDS = [
     "Epic Link",  # Epic Link MUST come before Epic Name in this list todo: need to fix this condition
     "Epic Name",  # This will only be available on issues that are an epic
